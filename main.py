5bbe7435af1963e62ae6815b17832b3418a0cb04
Gabi <gabriela.szczepankova@pcsupport.cz>
2021-08-05 12:20:23 +0200

    Working with json file

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import json
+
 print("Project number 1 - Text to morse code converter")
 
 user_text = input("Please insert text you want to convert: ")
+
+with open('morse-alphabet.json', "r") as file:
+    alphabet = json.load(file)
+    print(alphabet['a'])
 
